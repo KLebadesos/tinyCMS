62eb45e426fb4a8b3a6b6ed28661ac5b5400754f
Klebadesos <lebadesoskevin@gmail.com>
2020-03-19 01:00:27 +0800

    update category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Requests\Categories\CreateCategoryRequest;
+use App\Http\Requests\Categories\UpdateCategoryRequest;
 use App\Category;
 
 
@@ -68,9 +69,12 @@ class CategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Category $category)
     {
-        //
+        //same method if not using route model binding
+        //$data = Category::find($category);
+
+        return view('categories.create')->with('category',$category);
     }
 
     /**
@@ -80,9 +84,16 @@ class CategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update([
+            'name' => $request->name
+        ]);
+        $category->save();
+
+        session()->flash('success','Has been successfully updated');
+
+        return redirect(route('categories.index'));
     }
 
     /**
--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -4,7 +4,7 @@
 
 <div class="card card-default">
     <div class="card-header">
-    Categories
+        {{ isset($category) ? 'Edit category' : 'Create category' }}
     </div>
 
     <div class="card-body">
@@ -20,16 +20,17 @@
         </div>
     @endif
 
-        <form action="{{ route('categories.store') }}" method="POST">
+        <form action="{{ isset($category) ? route('categories.update',$category->id) : route('categories.store') }}" method="POST">
         @csrf
+        @method('PUT')
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" id="name" class="form-control" name="name">
+                <input type="text" id="name" class="form-control" name="name" value="{{ isset($category) ? $category->name : '' }}">
             </div>
 
             <div class="form-group">
                 <button class="btn btn-success">
-                Add category
+                {{ isset($category) ? 'Update category' : 'Add category' }}
                 </button>
             </div>
         </form>
