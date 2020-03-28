212920ae4b762bfcaeff8d40a410b7f8e3a1de68
Klebadesos <lebadesoskevin@gmail.com>
2020-03-29 06:38:20 +0800

    refactor error notif

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,18 +4,7 @@
 <div class="card card-default">
 <div class="card-header">{{ isset($post) ? 'Edit post' : 'Create Posts' }}</div>
   <div class="card-body">
-
-  @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul class="list-group">
-        @foreach ($errors->all() as $error )
-          <li class="list-group-item">
-            {{ $error }}
-          </li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
+    @include('partials.error')
 
     <form action="{{ isset($post) ? route('posts.update', $post->id) : route('posts.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
