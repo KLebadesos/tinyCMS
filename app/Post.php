e32e82359581ad3209024b489b7092b82b820011
Klebadesos <lebadesoskevin@gmail.com>
2020-03-30 23:36:03 +0800

    added relation post and tag

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use App\Tag;
 
 class Post extends Model
 {
@@ -32,5 +33,13 @@ class Post extends Model
 
      public function category(){
         return $this->belongsTo(Category::class);
+     }
+
+     public function tags(){
+        return $this->belongsToMany(Tag::class);
+     }
+
+     public function hasTag($tagId){
+         return in_array($tagId, $this->tags->pluck('id')->toArray());
      }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Tag extends Model
 {
     protected $fillable = ['name'];
+
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
+
+
